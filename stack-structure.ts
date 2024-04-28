a1135a93256c0d7e1be911f59dae879acc0da039
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-04-28 18:59:23 +0300

    stack peek method

--- stack-structure.ts
+++ stack-structure.ts
@@ -16,8 +16,16 @@ class Stack<T> {
         if (this.count === 0) {
             throw new Error('Stack is empty!');
         }
-        
+
         this.count--;
         return this.items.pop();
+    }
+
+    peek(): T | undefined {
+        if (this.count === 0) {
+            throw new Error('Stack is empty!');
+        }
+
+        return this.items[this.count - 1];
     }
 }
