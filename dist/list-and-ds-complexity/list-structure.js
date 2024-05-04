689d6b97521c58c2b5334de0b493e7b24379d253
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-04 11:29:53 +0300

    test the array list structure

--- dist/list-and-ds-complexity/list-structure.js
+++ dist/list-and-ds-complexity/list-structure.js
@@ -1,80 +1,65 @@
-class ArrayList<T> {
-    private items: T[];
-    private count: number;
-
+"use strict";
+class ArrayList {
+    items;
+    count;
     constructor() {
         this.items = new Array(2);
         this.count = 0;
     }
-
-    get Count(): number {
+    get Count() {
         return this.count;
     }
-
-    get(index: number): T {
+    get(index) {
         if (index < 0 || index >= this.count) {
             throw new RangeError('Index out of range');
         }
-
         return this.items[this.count - 1 - index];
     }
-
-    set(index: number, value: T) {
+    set(index, value) {
         if (index < 0 || index >= this.count) {
             throw new RangeError('Index out of range');
         }
-
         this.items[this.count - 1 - index] = value;
     }
-
-    Add(item: T): void {
+    Add(item) {
         if (this.count === this.items.length) {
-            this.resize()
+            this.resize();
         }
-
         this.items[this.count++] = item;
     }
-
-    RemoveAt(index: number): T {
+    RemoveAt(index) {
         if (index < 0 || index >= this.count) {
             throw new RangeError('Index out of range');
         }
-
         const removedItem = this.get(index);
         this.shift(index);
         this.count--;
-
         if (this.count <= this.items.length / 4) {
             this.shrink();
         }
-
         return removedItem;
     }
-
-    private resize(): void {
+    resize() {
         const newItems = new Array(this.items.length * 2);
         for (let i = 0; i < this.items.length; i++) {
             newItems[i] = this.items[i];
         }
         this.items = newItems;
     }
-
-    private shrink(): void {
+    shrink() {
         const newItems = new Array(this.items.length / 2);
         for (let i = 0; i < this.count; i++) {
             newItems[i] = this.items[i];
         }
         this.items = newItems;
     }
-
-    private shift(index: number): void {
+    shift(index) {
         for (let i = index; i < this.count - 1; i++) {
             this.items[this.count - 1 - i] = this.items[this.count - 2 - i];
         }
     }
 }
-
-const list = new ArrayList<number>();
+const list = new ArrayList();
 list.Add(1);
 list.Add(2);
 list.Add(3);
