b2cff7f2febae28c835e9c9927bba9d28d7b429e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-04 11:21:35 +0300

    getter by index

--- list-and-ds-complexity/list-structure.ts
+++ list-and-ds-complexity/list-structure.ts
@@ -10,4 +10,12 @@ class ArrayList<T> {
     get Count(): number {
         return this.count;
     }
+
+    get(index: number):T {
+        if (index < 0 || index >= this.count) {
+            throw new RangeError('Index out of range');
+        }
+
+        return this.items[this.count - 1 - index];
+    }
 }
