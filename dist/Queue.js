183569c98da04745e252ebf75cfe6b71f69765b4
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-09-16 00:47:01 +0300

    implement poll method

--- dist/Queue.js
+++ dist/Queue.js
@@ -7,7 +7,16 @@ class Queue {
     offer(element) {
         this.queue.push(element);
     }
+    poll() {
+        if (this.queue.length === 0) {
+            throw new RangeError('Queue is empty!');
+        }
+        else {
+            return this.queue.shift();
+        }
+    }
 }
 let queue = new Queue();
 queue.offer(1);
 console.log(queue);
+console.log(queue.poll());
--- src/Queue.ts
+++ src/Queue.ts
@@ -8,8 +8,17 @@ class Queue<E> {
     offer(element: E) {
         this.queue.push(element);
     }
+
+    poll(): E | undefined{
+        if (this.queue.length === 0) {
+            throw new RangeError('Queue is empty!');
+        } else {
+            return this.queue.shift();
+        }
+    }
 }
 
 let queue = new Queue<number>();
 queue.offer(1);
 console.log(queue);
+console.log(queue.poll());
