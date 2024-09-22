a9873b832b08e175978f6bc52b395d3e22964eda
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-09-22 10:19:07 +0300

    implements poll method

--- src/5-FasterQueue.ts
+++ src/5-FasterQueue.ts
@@ -8,6 +8,14 @@ class FasterQueue<E> {
     offer(element: E) {
         return this.queue.push(element);
     }
+
+    poll(): E | undefined{
+        if (this.queue.length === 0) {
+            throw new RangeError('Queue is empty!');
+        } else {
+            return this.queue.shift();
+        }
+    }
 }
 
 let fasterQueue: FasterQueue<number> = new FasterQueue<number>();
